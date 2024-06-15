a7e9c62da42473196af8e1d795c0c9dfbaaaed71
unknown <arvindaffa49@gmail.com>
2024-06-15 19:41:21 +0700

    feat:add capture photo and open gallery

---
+++ app/src/main/java/com/android/translationapp/BuildConfig.kt
@@ -0,0 +1,9 @@
+package com.android.translationapp
+
+object BuildConfig {
+    val DEBUG: Boolean = "true".toBoolean()
+    const val APPLICATION_ID: String = "com.dicoding.picodiploma.mycamera"
+    const val BUILD_TYPE: String = "debug"
+    const val VERSION_CODE: Int = 1
+    const val VERSION_NAME: String = "1.0"
+}
