182085216ec3b4d140cb8b70bc1a423b66a7185f
unknown <arvindaffa49@gmail.com>
2024-06-16 13:38:19 +0700

    feat:add translate feature

--- app/src/main/java/com/android/translationapp/ResultActivity.kt
+++ app/src/main/java/com/android/translationapp/ResultActivity.kt
@@ -24,6 +24,9 @@ class ResultActivity : AppCompatActivity() {
             binding.resultImage.setImageURI(it)
         }
 
+        val detectedText = intent.getStringExtra(EXTRA_RESULT)
+        binding.resultText.text = detectedText
+
     }
 
     companion object {
